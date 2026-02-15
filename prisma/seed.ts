8a0ea6a2badf0cdcd96f5b360f3853e42112838d
MyatSuMon253 <myatsumon2531999@gmail.com>
2026-02-15 22:41:52 +0630

    feat: seed user data with fakerjs

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,6 +2,8 @@ import { PrismaClient, Prisma } from "../src/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
 import * as bcrypt from "bcrypt";
+import { faker } from "@faker-js/faker";
+import { create } from "node:domain";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
@@ -11,33 +13,15 @@ const prisma = new PrismaClient({
   adapter,
 });
 
-const userData: Prisma.UserCreateInput[] = [
-  {
-    phone: "778661260",
+export function createRandomUser() {
+  return {
+    phone: faker.phone.number({ style: "international" }),
     password: "",
-    randToken: "sfwfx23rbkxg982ntxf87",
-  },
-  {
-    phone: "778661261",
-    password: "",
-    randToken: "sfwfx23rbkxg982ntxf87",
-  },
-  {
-    phone: "778661262",
-    password: "",
-    randToken: "sfwfx23rbkxg982ntxf87",
-  },
-  {
-    phone: "778661263",
-    password: "",
-    randToken: "sfwfx23rbkxg982ntxf87",
-  },
-  {
-    phone: "778661264",
-    password: "",
-    randToken: "sfwfx23rbkxg982ntxf87",
-  },
-];
+    randToken: faker.internet.jwt(),
+  };
+}
+
+export const userData = faker.helpers.multiple(createRandomUser, { count: 10 });
 
 async function main() {
   console.log("start seeding...");
