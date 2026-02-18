d1ea56a6c93cc3bf9700779f31cb2fe7041c1623
MyatSuMon253 <myatsumon2531999@gmail.com>
2026-02-18 15:21:19 +0630

    feat: verify otp with 2 min expiration time

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -8,8 +8,14 @@ import {
   getUserByPhone,
   updateOtp,
 } from "../services/authServices";
-import { checkOtpErrorIfSameDate, checkUserExist } from "../utils/auth";
+import {
+  checkOtpErrorIfSameDate,
+  checkOtpRow,
+  checkUserExist,
+} from "../utils/auth";
 import { generateOTP, generateToken } from "../utils/generate";
+import { prisma } from "../lib/prisma";
+import moment from "moment";
 
 export const register = [
   body("phone", "Invalid phone number")
@@ -40,7 +46,8 @@ export const register = [
     // Generate OTP & call OTP sending API
     // if sms OTP cannot be sent, response error
     // Save OTP to DB
-    const otp = generateOTP();
+    // const otp = generateOTP();
+    const otp = 123456;
     const salt = await bcrypt.genSalt(10);
     const hasedOtp = await bcrypt.hash(otp.toString(), salt);
     const token = generateToken();
@@ -99,13 +106,103 @@ export const register = [
   },
 ];
 
-export const verifyOtp = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  res.status(200).json({ message: "veriπfyOtp" });
-};
+export const verifyOtp = [
+  body("phone", "Invalid phone number")
+    .trim()
+    .notEmpty()
+    .matches("^[0-9]+$")
+    .isLength({ min: 5, max: 12 }),
+  body("otp", "Invalid otp")
+    .trim()
+    .notEmpty()
+    .matches("^[0-9]+$")
+    .isLength({ min: 6, max: 6 }),
+  body("token", "Invalid token").trim().notEmpty().escape(),
+  async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req).array({ onlyFirstError: true });
+
+    if (errors.length > 0) {
+      const error: any = new Error(errors[0].msg);
+      error.status = 400;
+      error.code = "ERR_INVALID";
+
+      return next(error);
+    }
+
+    const { phone, otp, token } = req.body;
+    const user = await getUserByPhone(phone);
+    checkUserExist(user);
+
+    const otpRow = await getOtpByPhone(phone);
+    checkOtpRow(otpRow);
+
+    const lastOtpVerify = new Date(otpRow!.updatedAt).toLocaleDateString();
+    const today = new Date().toLocaleDateString();
+    const isSameDate = lastOtpVerify === today;
+
+    // if otpVerify is in the same date and over limit
+    checkOtpErrorIfSameDate(isSameDate, otpRow!.error);
+    let result;
+
+    if (otpRow?.rememberToken !== token) {
+      const otpData = {
+        errorCount: 5,
+      };
+      result = await updateOtp(otpRow!.id, otpData);
+
+      const error: any = new Error("Invalid token");
+      error.status = 400;
+      error.code = "Error_Invalid";
+      return next(error);
+    }
+
+    // otp is expired 2 min
+    const isExpired = moment().diff(otpRow?.updatedAt, "minute") > 2;
+    if (isExpired) {
+      const error: any = new Error("Otp is expired");
+      error.status = 403;
+      error.code = "Error_Expired";
+      return next(error);
+    }
+
+    const isMatch = await bcrypt.compare(otp, otpRow!.otp);
+    // otp is wrong
+    if (!isMatch) {
+      // if otp error is first time today
+      if (!isSameDate) {
+        const otpData = {
+          error: 1,
+        };
+        await updateOtp(otpRow!.id, otpData);
+      } else {
+        // if otp error is not first time today
+        const otpData = {
+          error: { increment: 1 },
+        };
+        await updateOtp(otpRow!.id, otpData);
+      }
+      const error: any = new Error("Otp is incorrect");
+      error.status = 400;
+      error.code = "Error_Invalid";
+      return next(error);
+    }
+
+    const verifyToken = generateToken();
+    const otpData = {
+      verifyToken,
+      error: 0,
+      count: 1,
+    };
+
+    result = await updateOtp(otpRow!.id, otpData);
+
+    res.status(200).json({
+      message: "OTP is successfully verify",
+      phone: result.phone,
+      token: verifyToken,
+    });
+  },
+];
 
 export const confirmPassword = async (
   req: Request,
