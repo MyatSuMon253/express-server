ad28dfae99962944816204fb7eb7396233db2981
MyatSuMon253 <myatsumon2531999@gmail.com>
2026-02-21 02:38:09 +0630

    feat: set maintenance mode

---
+++ src/controllers/admin/systemController.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Response } from "express";
+
+import { CustomRequest } from "../../types";
+import { body, validationResult } from "express-validator";
+import { createError } from "../../utils/error";
+import { errorCode } from "../../config/errorCode";
+import { createOrUpdateSettingStatus } from "../../services/settingService";
+
+export const setMaintenance = [
+  body("mode", "Mode must be boolean").isBoolean(),
+  async (req: CustomRequest, res: Response, next: NextFunction) => {
+    const errors = validationResult(req).array({ onlyFirstError: true });
+
+    if (errors.length > 0) {
+      return next(createError(errors[0].msg, 400, errorCode.invalid));
+    }
+
+    const { mode } = req.body;
+    const value = mode ? "true" : "false";
+    const message = mode
+      ? "Successfully set maintenance mode."
+      : "Successfully turn off maintenance mode.";
+
+    await createOrUpdateSettingStatus("maintenance", value);
+
+    res.status(200).json({ message });
+  },
+];
--- src/controllers/admin/userController.ts
+++ src/controllers/admin/userController.ts
@@ -8,7 +8,7 @@ export const getAllUsers = (
   next: NextFunction,
 ) => {
   const user = req.user;
-  console.log("requserklr", req.user);
+
   res
     .status(200)
     .json({ message: req.t("welcome"), currentUserRole: user.role });
