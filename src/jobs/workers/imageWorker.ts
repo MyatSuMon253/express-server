df1d3cca9363b331672c2ade731d5be2e07a3f68
MyatSuMon253 <myatsumon2531999@gmail.com>
2026-05-02 22:54:13 +0630

    feat: add queue and worker

---
+++ src/jobs/workers/imageWorker.ts
@@ -0,0 +1,38 @@
+import { Worker } from "bullmq";
+import { Redis } from "ioredis";
+import sharp from "sharp";
+import path from "path";
+
+const connection = new Redis({
+  host: process.env.REDIS_HOST,
+  port: parseInt(process.env.REDIS_PORT!),
+  password: process.env.REDIS_PASSWORD,
+  maxRetriesPerRequest: null,
+});
+
+// create worker to process image optimization job
+const imageWorker = new Worker(
+  "imageQueue",
+  async (job) => {
+    const { filePath, fileName } = job.data;
+    const optimizedImagePath = path.join(
+      __dirname,
+      "../../..",
+      "/uploads/optimized",
+      fileName,
+    );
+    await sharp(filePath)
+      .resize(200, 200)
+      .webp({ quality: 50 })
+      .toFile(optimizedImagePath);
+  },
+  { connection },
+);
+
+imageWorker.on("completed", (job) => {
+  console.log(`job completed with result ${job.id}`);
+});
+
+imageWorker.on("failed", (job: any, error) => {
+  console.log(`job ${job.id} failed with ${error.message} `);
+});
